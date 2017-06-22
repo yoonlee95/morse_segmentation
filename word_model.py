f84acb9c1830591663b6c335b3f511e6e1c1380e
John Lee <yoonlee95@gmail.com>
2017-06-22 00:39:04 -0500

    now sort the words

--- word_model.py
+++ word_model.py
@@ -13,6 +13,7 @@ class WORDMODEL(object):
         if type == "fasttext":
             print "loading fasttext model"
             self.model = fasttext.load_model(data)
+            print "Model Loaded"
             if batch != -1:
                 self.words = (list(self.model.words))[:batch]
             else:
@@ -21,18 +22,27 @@ class WORDMODEL(object):
 
             print "loading word2vec model"
             self.model = KeyedVectors.load_word2vec_format(data, binary=False)
+            print "Model Loaded"
 
             self.words = []
+            full_word = []
             if batch != -1:
-                counter = 0
-                for (k, _) in self.model.vocab.iteritems():
-                    counter += 1
-                    if counter == batch:
-                        break
-                    self.words.append(k)
+                for (k, obj) in self.model.vocab.iteritems():
+                    full_word.append((k, obj.count))
+
+                print "Sorting Start"
+
+                full_word = sorted(full_word, key=lambda x: x[1], reverse=True)
+
+                for i in range(batch):
+                    self.words.append(full_word[i][0])
+
+                print "Sorting Done"
+
             else:
                 for (k, _) in self.model.vocab.iteritems():
                     self.words.append(k)
+            del full_word
 
         print "loading done"
 
